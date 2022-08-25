528dff60fa3c3d6f806b36b627741f1edf82c77a
Thomas Meckel <tmeckel@users.noreply.github.com>
2022-08-25 15:55:24 +0200

    Changes to local_extensions.py: * added filters capitalize, go_module_version, go_module_name

--- local_extensions.py
+++ local_extensions.py
@@ -1,10 +1,37 @@
 from jinja2.environment import Environment
 from jinja2.ext import Extension
+import re
+
 
 def is_truthy(v):
-    return v.lower() in [ 'true', '1', 'yes', 'y' ]
+    return v.lower() in ["true", "1", "yes", "y"]
+
+
+def capitalize(v):
+    return v.capitalize()
+
+
+def go_module_version(v):
+    if not v:
+        raise ValueError("Value is empty")
+    if v.lower().startswith("v"):
+        v = v[1:]
+    version = v.split(".")
+    major = int(version[0])
+    if major > 1:
+        return "/v%s" % major
+    else:
+        return ""
+
+
+def go_module_name(v):
+    return re.sub("/v[0-9]+$", "", v)
+
 
 class FilterExtension(Extension):
     def __init__(self, environment: Environment) -> None:
         super().__init__(environment)
         environment.tests["truthy"] = is_truthy
+        environment.filters["capitalize"] = capitalize
+        environment.filters["go_module_version"] = go_module_version
+        environment.filters["go_module_name"] = go_module_name
