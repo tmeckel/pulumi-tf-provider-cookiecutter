b9571c6d03e64ecf95ba5fc5d4a2e07560f37d2c
Thomas Meckel <tmeckel@users.noreply.github.com>
2022-08-26 12:36:14 +0200

    Changes to hooks/pre_gen_project.py: * added support for SHA commit references while verifying the referenced provider version * validating assigned meta data category * validating Terraform SDK version number

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -9,19 +9,42 @@ def _error_exit(msg):
 
 
 PROVIDER_NAME_REGEX = r"^[a-zA-Z0-9]+$"
-VERION_REGEX = r"^([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$"
-
 provider_name = "{{ cookiecutter.terraform_provider_name }}"
-
 if re.match(r"terraform-?provider-?", provider_name):
     _error_exit("provider name MUST NOT start with terraform-provider-")
 if not re.match(PROVIDER_NAME_REGEX, provider_name):
     _error_exit("%s IS NOT a valid terraform provider name!" % provider_name)
 
-terraform_provider_version = "{{ cookiecutter.terraform_provider_version }}"
 
-if not re.match(VERION_REGEX, terraform_provider_version):
-    _error_exit("%s IS NOT a valid version!" % terraform_provider_version)
+VERSION_REGEX = r"^([0-9]+)(\.[0-9]+)?(\.[0-9]+)?$|[0-9a-f]{40}"
+terraform_provider_version = "{{ cookiecutter.terraform_provider_version_or_commit }}"
+if not re.match(VERSION_REGEX, terraform_provider_version):
+    _error_exit(
+        "%s IS NOT a valid version! Eiter a version 0.0.0 or a commit hash must be specified."
+        % terraform_provider_version
+    )
 
-if "{{ cookiecutter.create_shim }}".lower() in ["true", "1", "yes", "y"] and not "{{ cookiecutter.terraform_provider_internal_package }}":
-    _error_exit("terraform_provider_internal_package MUST BE set if using a SHIM!")
+VALID_CATEGORIES = [
+    "cloud",
+    "database",
+    "infrastructure",
+    "monitoring",
+    "network",
+    "utility",
+    "versioncontrol",
+]
+if not "{{ cookiecutter.provider_category }}" in VALID_CATEGORIES:
+    _error_exit(f"provider_category MUST BE one of {VALID_CATEGORIES}")
+
+SDK_VERSION_REGEX = r"^\s*[12]\s*$"
+terraform_sdk_version = "{{cookiecutter.terraform_sdk_version | trim }}"
+if not re.match(SDK_VERSION_REGEX, terraform_sdk_version):
+    _error_exit(
+        "%s IS NOT a valid Terrafom SDK version! Only 1 or 2 are allowed as values"
+        % terraform_sdk_version
+    )
+
+if int(terraform_sdk_version) != 2:
+    _error_exit(
+        f"Currently only Terraform providers using Terraform SDK V2 are supported"
+    )
