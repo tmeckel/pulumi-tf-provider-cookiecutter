eaecd09ddd092dca87a0fa86c0a500057b935675
Thomas Meckel <tmeckel@users.noreply.github.com>
2022-08-26 12:49:32 +0200

    Changes to {{cookiecutter.provider}}/provider/shim/shim.go: * changed code according to new cookiecutter template property names

--- {{cookiecutter.provider}}/provider/shim/shim.go
+++ {{cookiecutter.provider}}/provider/shim/shim.go
@@ -2,10 +2,10 @@ package shim
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"{{ cookiecutter.terraform_provider_source }}/{{ cookiecutter.terraform_provider_internal_package }}"
+	"{{ cookiecutter.terraform_provider_module }}/{{ cookiecutter.terraform_provider_package_name }}"
 )
 
 func NewProvider() *schema.Provider {
-	{% set list = cookiecutter.terraform_provider_internal_package.split('/') -%}
+	{% set list = cookiecutter.terraform_provider_package_name.split('/') -%}
 	return {{ list[-1] }}.Provider()
 }
