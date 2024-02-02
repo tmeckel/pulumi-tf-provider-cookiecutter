96888599c22502a3658630fc915d7a3d7d037e04
Thomas Meckel <tmeckel@users.noreply.github.com>
2024-02-02 16:53:04 +0000

    ✨feat: add function get_go_version to retrieve the installed Go language version to local_extensions.py
    Closes: #23

--- local_extensions.py
+++ local_extensions.py
@@ -1,9 +1,10 @@
 import re
-import requests
-from packaging import version
+from subprocess import PIPE, Popen
 
+import requests
 from jinja2.environment import Environment
 from jinja2.ext import Extension
+from packaging import version
 
 
 def is_truthy(v):
@@ -51,14 +52,16 @@ def version_major(v):
 def version_minor(v):
     return version.parse(v).minor
 
-def get_latest_release(repo, owner = "pulumi"):
+
+def get_latest_release(repo, owner="pulumi"):
     release_url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
     response = requests.get(release_url)
     data = response.json()
 
     return data["tag_name"]
 
-def get_latest_release_commit(repo, owner = "pulumi"):
+
+def get_latest_release_commit(repo, owner="pulumi"):
     tag = get_latest_release(repo, owner)
 
     tag_info_url = f"https://api.github.com/repos/{owner}/{repo}/git/ref/tags/{tag}"
@@ -67,11 +70,39 @@ def get_latest_release_commit(repo, owner = "pulumi"):
 
     return tag_info["object"]["sha"]
 
+
+def get_go_version():
+    """
+    Returns the Go language version installed on the system as a version.Version object.
+
+    This function executes the 'go env GOVERSION' command using Popen and parses the version string
+    into a `packaging.version.Version` object.
+
+    Returns:
+        version.Version: The Go version as a `packaging.version.Version` object.
+
+    Raises:
+        Exception: If the command fails to execute or returns an error.
+    """
+    process = Popen(
+        ["go", "env", "GOVERSION"],
+        stdout=PIPE,
+        text=True,
+    )
+    stdout, stderr = process.communicate()
+    if process.returncode == 0:
+        go_version_str = stdout.strip().lstrip("go")
+        return version.parse(go_version_str)
+    else:
+        raise Exception(f"Failed to get go version: {stderr}")
+
+
 class LocalExtension(Extension):
     def __init__(self, environment: Environment) -> None:
         super().__init__(environment)
         environment.globals["get_latest_release"] = get_latest_release
         environment.globals["get_latest_release_commit"] = get_latest_release_commit
+        environment.globals["get_go_version"] = get_go_version
         environment.filters["truthy"] = is_truthy
         environment.filters["capitalize"] = capitalize
         environment.filters["go_module_version"] = go_module_version
