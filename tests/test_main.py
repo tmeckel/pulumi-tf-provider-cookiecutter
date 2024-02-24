9724b8e7318ea27d9646ed0dc07b8f98564c885f
Thomas Meckel <14177833+tmeckel@users.noreply.github.com>
2024-02-24 18:04:58 +0100

    feat: add support for upstream directory in provider repo
    * feat: add is_commit_hash filter to local_extensions.py
    
    * 🎉feat: add support to detect if the wrapped Terraform provider must be cloned into a local upstream repository to enable tfgen to resolve documenation
    
    Closes: #38
    
    * test: added tests using ncloud Terraform provider

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,38 +11,44 @@ _logger = logging.getLogger()
 
 
 @pytest.mark.parametrize(
-    argnames="replay_file, compile, patches",
+    argnames="replay_file, patches",
     argvalues=[
         (
             "tests/replays/configcat/v1/pulumi-tf-provider-cookiecutter-configcat.json",
-            True,
             None,
         ),
         (
             "tests/replays/configcat/v4/pulumi-tf-provider-cookiecutter-configcat-v4.json",
-            True,
             [
                 "tests/replays/configcat/v4/shim.go.patch",
             ],
         ),
         (
             "tests/replays/zscaler/zpa/pulumi-tf-provider-cookiecutter-zscaler-zpa.json",
-            True,
             [
                 "tests/replays/zscaler/zpa/resources.go.patch",
             ],
+        ),
+        (
+            "tests/replays/ncloud/v2/pulumi-tf-provider-cookiecutter.json",
+            None,
+        ),
+        (
+            "tests/replays/ncloud/commit/pulumi-tf-provider-cookiecutter.json",
+            None,
         ),
     ],
     ids=[
         "configcat",
         "configcat-v4",
         "zscaler-zpa",
+        "ncloud-v2",
+        "ncloud-using-commit",
     ],
 )
 def test_replay_configuration(
     tmp_path_factory,
     replay_file: str,
-    compile: bool,
     patches: List[str],
 ):
     md5 = hashlib.md5(replay_file.encode())
@@ -55,32 +61,32 @@ def test_replay_configuration(
         replay=replay_file,
         output_dir=out_dir,
     )
-    if compile:
-        for patch in patches or []:
-            with open(patch) as f:
-                _logger.info("Apply patch %s to project in %s", patch, project_dir)
-                git = Popen(
-                    args=[
-                        "git",
-                        "apply",
-                        "--ignore-space-change",
-                        "--ignore-whitespace",
-                    ],
-                    cwd=project_dir,
-                    stdin=f,
-                    stderr=subprocess.PIPE,
-                )
-                git.wait()
-                if git.returncode != 0:
-                    pytest.fail(git.stderr.read().decode())
 
-        _logger.info("Compiling project in %s", project_dir)
+    for patch in patches or []:
+        with open(patch) as f:
+            _logger.info("Apply patch %s to project in %s", patch, project_dir)
+            git = Popen(
+                args=[
+                    "git",
+                    "apply",
+                    "--ignore-space-change",
+                    "--ignore-whitespace",
+                ],
+                cwd=project_dir,
+                stdin=f,
+                stderr=subprocess.PIPE,
+            )
+            git.wait()
+            if git.returncode != 0:
+                pytest.fail(git.stderr.read().decode())
 
-        make = Popen(
-            args=["make", "provider"],
-            cwd=project_dir,
-            stderr=subprocess.PIPE,
-        )
-        make.wait()
-        if make.returncode != 0:
-            pytest.fail(make.stderr.read().decode())
+    _logger.info("Compiling project in %s", project_dir)
+
+    make = Popen(
+        args=["make", "provider"],
+        cwd=project_dir,
+        stderr=subprocess.PIPE,
+    )
+    make.wait()
+    if make.returncode != 0:
+        pytest.fail(make.stderr.read().decode())
